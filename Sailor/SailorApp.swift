fb61fca76a2823ed028a2a82f5b06ef489dc7635
gordon.aspin@gmail.com <gordon.aspin@gmail.com>
2025-01-08 17:32:00 -0500

    Initial Commit

---
+++ Sailor/SailorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SailorApp.swift
+//  Sailor
+//
+//  Created by Gordon Aspin on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SailorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SailorTests/SailorTests.swift
@@ -0,0 +1,17 @@
+//
+//  SailorTests.swift
+//  SailorTests
+//
+//  Created by Gordon Aspin on 1/8/25.
+//
+
+import Testing
+@testable import Sailor
+
+struct SailorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
