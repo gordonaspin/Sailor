2086fef796da5d785fbc4a038c7ed3d1f767d305
gordon.aspin@gmail.com <gordon.aspin@gmail.com>
2025-01-18 10:25:32 -0500

    Update text

--- Sailor/Views/LocationDeniedView.swift
+++ Sailor/Views/LocationDeniedView.swift
@@ -5,23 +5,23 @@ struct LocationDeniedView: View {
         ContentUnavailableView(label: {
             Label("Location Services", image: "launchScreen")
         },
-                               description: {
+        description: {
             Text("""
 1. Tab the button below and go to "Privacy and Security"
 2. Tap on "Location Services"
-3. Locate the "MyTrips" app and tap on it
+3. Locate the "Sailor" app and tap on it
 4. Change the setting to "While Using the App"
 """)
             .multilineTextAlignment(.leading)
         },
-                               actions: {
+        actions: {
             Button(action: {
                 UIApplication.shared.open(
                     URL(string: UIApplication.openSettingsURLString)!,
                     options: [:],
-                    completionHandler: nil
-                )
-            }) {
+                    completionHandler: nil)
+            })
+            {
                 Text("Open Settings")
             }
             .buttonStyle(.borderedProminent)
