6f0bcf125a889e6c15ecd6bbd70640093f79aed4
Gordon Aspin <gordon.aspin@gmail.com>
2025-01-22 14:29:48 -0500

    start/stop tracking

---
+++ Sailing/Views/Wind/ArrowView.swift
@@ -0,0 +1,18 @@
+//
+//  ArrowView.swift
+//  Sailing
+//
+//  Created by Gordon Aspin on 1/22/25.
+//
+
+import SwiftUI
+
+struct ArrowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArrowView()
+}
