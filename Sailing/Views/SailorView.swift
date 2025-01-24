9b7d1dae30b0540db57fed948f9bc9a97a68407e
Gordon Aspin <gordon.aspin@gmail.com>
2025-01-24 11:21:12 -0500

    animation

--- Sailing/Views/SailorView.swift
+++ Sailing/Views/SailorView.swift
@@ -12,106 +12,71 @@ struct SailorView: View {
         AnyView(AllInstrumentsLayoutView()),
         AnyView(SpeedHeadingHeelPitchInstrumentsLayoutView()),
         AnyView(SpeedHeadingInstrumentsLayoutView())
-        ]
-
+    ]
+    
     @Environment(LocationManager.self) var locationManager
     @Environment(MotionManager.self) var motionManager
     @Environment(WeatherManager.self) var weatherManager
+    @Environment(\.colorScheme) var colorScheme
     @State var isRaceTimerPresented: Bool = false
     @State var viewIndex: Int = 0
     @State private var offset: CGFloat = 0
     @State private var backgroundColor: Color = .clear
-
+    @State private var scaleFactor: CGFloat = 1
+    
     var body: some View {
         GeometryReader { geometry in
             
             VStack {
-                if !isRaceTimerPresented {
-                    ZStack {
-                        MapView()
+                ZStack {
+                    MapView()
+                    if !isRaceTimerPresented {
                         views[viewIndex]
-                            .animation(.easeInOut(duration: 2.0), value: offset)
-                            .gesture(
-                                DragGesture()
-                                    .onChanged { gesture in
-                                        offset = gesture.translation.width
-                                        backgroundColor = Color.gray.opacity(0.5)
-                                    }
-                                    .onEnded { gesture in
-                                        if gesture.predictedEndTranslation.width < -geometry.size.width / 8 {
-                                            nextView()
-                                        } else if gesture.predictedEndTranslation.width > geometry.size.width / 8 {
-                                            prevView()
-                                        } else {
-                                            offset = 0
-                                        }
-                                        backgroundColor = Color.clear
-                                    }
-                            )
+                            .offset(x: offset)
+                            .scaleEffect(scaleFactor)
                             .background(backgroundColor)
+                            .onAppear() {
+                                print("\(Date().toTimestamp) -  \(#file) \(#function) Map & DefaultLayoutView onAppear, start tracking")
+                                locationManager.startTracking()
+                                motionManager.startTracking()
+                            }
                     }
-                    .onAppear() {
-                        print("\(Date().toTimestamp) -  \(#file) \(#function) Map & DefaultLayoutView onAppear, start tracking")
-                        locationManager.startTracking()
-                        motionManager.startTracking()
+                    else {
+                        RaceTimerView(isPresented: $isRaceTimerPresented, viewIndex: $viewIndex)
+                            .background(colorScheme == .dark ? Color.black : Color.white)
+                            .offset(x: offset)
+                            .scaleEffect(scaleFactor)
+                            .onAppear {
+                                print("\(Date().toTimestamp) -  \(#file) \(#function) RaceTimerView onAppear, stop tracking")
+                                locationManager.stopTracking()
+                                motionManager.stopTracking()
+                            }
                     }
-                }
-                else {
-                    RaceTimerView(isPresented: $isRaceTimerPresented, viewIndex: $viewIndex)
-                        .animation(.easeInOut(duration: 2.0), value: offset)
-                        .gesture(
-                            DragGesture()
-                                .onChanged { gesture in
-                                    offset = gesture.translation.width
-                                    backgroundColor = Color.gray.opacity(0.5)
-                                }
-                                .onEnded { gesture in
-                                    if gesture.predictedEndTranslation.width < -geometry.size.width / 8 {
-                                        nextView()
-                                    } else if gesture.predictedEndTranslation.width > geometry.size.width / 8 {
-                                        prevView()
-                                    } else {
-                                        offset = 0
-                                    }
-                                    backgroundColor = Color.clear
-                                }
-                        )
-                        .background(backgroundColor)
-                        .onAppear {
-                            print("\(Date().toTimestamp) -  \(#file) \(#function) RaceTimerView onAppear, stop tracking")
-                            locationManager.stopTracking()
-                            motionManager.stopTracking()
-                        }
                 }
             }
-            /*.swipe(
-                left: {
-                    if isRaceTimerPresented {
-                        viewIndex = views.count - 1
-                        isRaceTimerPresented = false
+            .animation(.spring(), value: offset)
+            .gesture(
+                DragGesture()
+                    .onChanged { gesture in
+                        offset = gesture.translation.width
+                        if abs(gesture.translation.width) > geometry.size.width / 8 {
+                            backgroundColor = Color.gray.opacity(0.9)
+                            scaleFactor = 0.9
+                        }
                     }
-                    else {
-                        viewIndex = viewIndex - 1
+                    .onEnded { gesture in
+                        if gesture.translation.width < -geometry.size.width / 8 {
+                            offset = geometry.size.width
+                            nextView()
+                        } else if gesture.translation.width > geometry.size.width / 8 {
+                            offset = -geometry.size.width
+                            prevView()
+                        }
+                        offset = 0.0
+                        scaleFactor = 1.0
+                        backgroundColor = Color.clear
                     }
-                    if viewIndex == -1 {
-                        isRaceTimerPresented = true
-                    }
-                    print("swipe left : \(viewIndex) \(isRaceTimerPresented)")
-                },
-                right: {
-                    if isRaceTimerPresented {
-                        viewIndex = 0
-                        isRaceTimerPresented = false
-                    }
-                    else {
-                        viewIndex = viewIndex + 1
-                    }
-                    if viewIndex == views.count {
-                        isRaceTimerPresented = true
-                    }
-                    print("swipe right : \(viewIndex) \(isRaceTimerPresented)")
-                }
-            )*/
+            )
         }
     }
     private func nextView() {
