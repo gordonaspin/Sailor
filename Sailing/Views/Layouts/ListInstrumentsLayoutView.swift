d7bb8457607b629b4b0469f08c68ba40f9c6c11b
Gordon Aspin <gordon.aspin@gmail.com>
2025-04-17 12:44:58 -0400

    Flags, Channel Markers, Permanent Marks

---
+++ Sailing/Views/Layouts/ListInstrumentsLayoutView.swift
@@ -0,0 +1,36 @@
+//
+//  DefaultInstrumentsLayoutView.swift
+//  Sailing
+//
+//  Created by Gordon Aspin on 1/18/25.
+//
+
+import SwiftUI
+
+struct ListInstrumentsLayoutView: View {
+    private let instruments: [AnyView] = [
+        AnyView(BoatSpeedView()),
+        AnyView(BoatHeadingView()),
+        AnyView(WindSpeedView()),
+        AnyView(WindDirectionView()),
+        AnyView(ApparentWindSpeedView()),
+        AnyView(ApparentWindAngleView()),
+        AnyView(HeelAngleView()),
+        AnyView(PitchAngleView())
+    ]
+    var body: some View {
+        InstrumentsLayoutView(instruments: instruments)
+    }
+}
+
+#Preview {
+    struct Preview: View {
+        var body: some View {
+            AllInstrumentsLayoutView()
+                .environment(LocationManager())
+                .environment(MotionManager())
+                .environment(WeatherManager())
+        }
+    }
+    return Preview()
+}
